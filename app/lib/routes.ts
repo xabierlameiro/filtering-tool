049b1b6b642343223c502ef6347b1e96c5673dc7
Xabier Lameiro <xabierlca@ext.inditex.com>
2024-03-19 05:07:36 +0100

    feat: auth (WIP)

--- app/lib/routes.ts
+++
@@ -1,3 +0,0 @@
-export const ROOT = '/';
-export const PUBLIC_ROUTES = ['/'];
-export const DEFAULT_REDIRECT = '/protected';
--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,22 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-    const session = req.cookies.get('authjs.session-token')
-    if (!session) {
-        return NextResponse.redirect(new URL('/api/auth/signin', req.nextUrl))
-    }
+  const session = req.cookies.get("authjs.session-token");
+  if (!session) {
+    return NextResponse.redirect(new URL("/api/auth/signin", req.nextUrl));
+  }
 }
 
 export const config = {
-    matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - api (API routes)
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         */
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    ],
-}
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
