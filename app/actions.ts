e742acf4408e1c2cf1ee6fdcb8b94c10762ae1b0
Xabier Lameiro <xabierlca@ext.inditex.com>
2024-03-19 22:26:51 +0100

    chore: add owner_email on candidates table

--- app/actions.ts
+++ app/actions.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { put } from "@vercel/blob";
 import { UUID } from "crypto";
+import { auth } from './auth'
 
 const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
 const ACCEPTED_TYPES = ["application/pdf"];
@@ -54,6 +55,8 @@ export async function createNewCandidate(
   prevState: { url?: string },
   formData: FormData,
 ) {
+  const session = await auth()
+
   const parse = schema.safeParse({
     technology: formData.get("technology"),
     name: formData.get("name"),
@@ -70,8 +73,8 @@ export async function createNewCandidate(
   try {
     // We need insert the owner
     await sql`INSERT INTO candidates
-        (id,  name, technology, created_at, cv_url)
-        VALUES (${parse.data.id}, ${parse.data.name}, ${parse.data.technology}, ${parse.data.created_at}, ${parse.data.cv_url})`;
+        (id, name, owner_email, technology, created_at, cv_url)
+        VALUES (${parse.data.id}, ${parse.data.name}, ${session?.user?.email}, ${parse.data.technology}, ${parse.data.created_at}, ${parse.data.cv_url})`;
 
     const BlobFile = formData.get("cv_binary") as Blob;
     const file = await BlobFile.arrayBuffer();
